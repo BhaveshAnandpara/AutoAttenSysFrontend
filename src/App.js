25cabe3931abe3925aefdccbad4ed3ebf4f92b2e
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-12-30 17:48:52 +0530

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import EnrollUser from './Pages/EnrollUser';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <EnrollUser/>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/enrollUser' element={ <EnrollUser/> } />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
